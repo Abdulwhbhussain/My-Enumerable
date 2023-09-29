2bf9c37171a072f8e96919cb319b5ee5d1e05d47
Errytagedesign <errytage@gmail.com>
2023-09-29 08:57:47 +0100

    Add min, max an sort

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -15,3 +15,27 @@ module MyEnumerable
     result
   end
 end
+
+def max()
+  max = nil
+  each { |item| max = item if max.nil? || item > max }
+  max
+end
+
+def min()
+  min = nil
+  each { |item| min = item if min.nil? || item < min }
+  min
+end
+
+def sort()
+  result = []
+  each { |item| result << item }
+  result.sort do |a, b|
+    if block_given?
+      yield a, b
+    else
+      a <=> b
+    end
+  end
+end
