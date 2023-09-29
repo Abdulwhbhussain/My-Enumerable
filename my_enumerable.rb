786544f4eb7ab65f43669bc2b04e34e808feb415
WHB901 <abdulwhbhussain@gmail.com>
2023-09-29 14:59:53 +0500

    Add Class and Module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -20,13 +20,13 @@ module MyEnumerable
     each { |item| max = item if max.nil? || item > max }
     max
   end
-  
+
   def min()
     min = nil
     each { |item| min = item if min.nil? || item < min }
     min
   end
-  
+
   def sort()
     result = []
     each { |item| result << item }
