d39ffefbe3f1631ec2c5ea84564ba853c3eee5e7
WHB901 <abdulwhbhussain@gmail.com>
2023-09-29 14:36:21 +0500

    Add Class and Module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -4,38 +4,27 @@ module MyEnumerable
     true
   end
 
-  def any?()
-    each { |item| return true if yield item }
-    false
+  def max()
+    max = nil
+    each { |item| max = item if max.nil? || item > max }
+    max
   end
-
-  def filter()
+  
+  def min()
+    min = nil
+    each { |item| min = item if min.nil? || item < min }
+    min
+  end
+  
+  def sort()
     result = []
-    each { |item| result << item if yield item }
-    result
-  end
-end
-
-def max()
-  max = nil
-  each { |item| max = item if max.nil? || item > max }
-  max
-end
-
-def min()
-  min = nil
-  each { |item| min = item if min.nil? || item < min }
-  min
-end
-
-def sort()
-  result = []
-  each { |item| result << item }
-  result.sort do |a, b|
-    if block_given?
-      yield a, b
-    else
-      a <=> b
+    each { |item| result << item }
+    result.sort do |a, b|
+      if block_given?
+        yield a, b
+      else
+        a <=> b
+      end
     end
   end
 end
